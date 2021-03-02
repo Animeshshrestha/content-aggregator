f61c9e5452977fbc021d8dfba05f56c72745c245
Animesh <bharatshrestha925@gmail.com>
2021-03-02 16:26:30 +0545

    first commit: scraping web content finished

---
+++ news_scrapping/news_scrapping/items.py
@@ -0,0 +1,12 @@
+import scrapy
+
+
+class NewsScrappingItem(scrapy.Item):
+
+    category = scrapy.Field()
+    title = scrapy.Field()
+    link = scrapy.Field()
+    description = scrapy.Field()
+    images_link = scrapy.Field()
+
+
